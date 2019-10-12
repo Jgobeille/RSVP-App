7f1f0209d9eeab444e2a848cc8fdf139aceb13ed
Jamie Gobeille <djent34@gmail.com>
2019-10-12 19:50:39 -0400

    add remove feature and refactored submit listener function

--- RSVP App/app.js
+++ RSVP App/app.js
@@ -2,10 +2,7 @@ const form = document.querySelector('#registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = input.value;
-    input.value = "";
+function createLi(text) {
     const li = document.createElement('li');
     li.textContent = text;
     const label = document.createElement('label');
@@ -14,6 +11,19 @@ form.addEventListener('submit', (e) => {
     checkbox.type = 'checkbox';
     label.appendChild(checkbox);
     li.appendChild(label);
+    const button = document.createElement('button');
+    button.textContent = 'remove';
+    li.appendChild(button);
+    return li;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = "";
+
+    const li = createLi(text);
+    
     ul.appendChild(li);
     
 });
@@ -28,5 +38,13 @@ ul.addEventListener('change', (e) => {
         
     } else {
         listItem.className = '';
+    }
+});
+
+ul.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
+        const li = e.target.parentNode;
+        const ul = li.parentNode;
+        ul.removeChild(li);
     }
 });
